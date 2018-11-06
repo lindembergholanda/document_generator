61fc4e3968570ab80680df15afb7ec8fa01be696
Lindemberg <lindemberg.holanda@gmail.com>
2018-11-05 21:42:08 -0300

    Add validate CNPJ

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,7 +5,12 @@ class Company < ApplicationRecord
     has_many :company_contacts, inverse_of: :company, dependent: :destroy
 
     validates :name, :trade_name, :cnpj, presence: true
+    validate :validate_cnpj, unless: Proc.new { |company| company.cnpj.blank? }
 
     accepts_nested_attributes_for :company_address, allow_destroy: true, reject_if: proc { |attributes| not attributes['id'].blank? }
     accepts_nested_attributes_for :company_contacts, allow_destroy: true, reject_if: proc { |attributes| not attributes['id'].blank? }
+
+    def validate_cnpj
+        self.errors.add(:cnpj, I18n.t('errors.messages.invalid')) unless ValidateCnpj.valid? self.cnpj
+    end
 end
